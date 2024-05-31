09713205ca81c6bfb7783d9ec7e1caeb0800621b
Roni Ardiyanto <roon.ardiyanto@gmail.com>
2024-05-31 14:49:51 +0700

    :zap: feat(app): add customButtonEvent gtm and ga

--- src/components/features/BentoPageFeatures/index.tsx
+++ src/components/features/BentoPageFeatures/index.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import { Container } from '@mantine/core';
+import { Button, Container } from '@mantine/core';
+import { sendGAEvent, sendGTMEvent } from '@next/third-parties/google';
 
 import { BentoCard } from '@/components/ui/BentoCard';
 
 const BentoPageFeatures: React.FC = () => (
   <Container w="100%">
     <BentoCard />
+    <Button
+      onClick={() => {
+        sendGAEvent('event', 'customButtonClicked', { value: 'Klik 1A' });
+        sendGAEvent({ event: 'customButtonClicked', value: 'Klik 1B' });
+        sendGTMEvent({ event: 'customButtonClicked', value: 'Klik 1C' });
+      }}
+    >
+      Coba Test GTA Event dan GTM
+    </Button>
   </Container>
 );
 
