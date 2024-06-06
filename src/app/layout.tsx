2c35a85e8516306c810cd2a5440674490af8da09
Roni Ardiyanto <roon.ardiyanto@gmail.com>
2024-06-06 12:56:00 +0700

    :zap: feat(app): metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,55 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+import { siteConfig } from '@/config/site';
 
 type Props = {
   children: ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [
+      {
+        url: '/meta.jpeg',
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    title: 'Roni Ardiyanto - Software Developer',
+    description: siteConfig.description,
+    images: [
+      {
+        url: '/meta.jpeg',
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+    card: 'summary_large_image',
+  },
 };
 
 // Since we have a `not-found.tsx` page on the root, a layout file
---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Roni Ardiyanto',
+  url: 'https://roniar.dev',
+  description:
+    'Front-en dev by day, aspiring infra wizard by night. Always seeking new challenges and opportunities to learn..',
+  links: {
+    github: 'https://github.com/infrasync',
+    linkedin: 'https://www.linkedin.com/in/roniardiyanto/',
+    medium: 'https://medium.com/@roniardiyanto',
+    discord: 'https://discord.com/users/infrasync',
+  },
+};
