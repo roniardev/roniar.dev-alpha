d599f2c932910031d515671d5cddeda6793995f8
Roni Ardiyanto <roon.ardiyanto@gmail.com>
2024-06-11 11:17:30 +0700

    :zap: feat(app): change intro profile photo to cloudinary

--- src/components/features/HomePageFeatures/sections/Introduction/index.tsx
+++ src/components/features/HomePageFeatures/sections/Introduction/index.tsx
@@ -22,13 +22,33 @@ import {
 } from '@tabler/icons-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import profileImage from 'public/images/profil.jpg';
 
 import classes from './styles.module.css';
 
 import { FadeDownShow } from '@/components/ui/FadeDownShow';
 import { FadeInShow } from '@/components/ui/FadeInShow';
 import { FadeUpShow } from '@/components/ui/FadeUpShow';
+
+const shimmer = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str);
+
+const dataUrl = `data:image/svg+xml;base64,${toBase64(shimmer(200, 200))}`;
 
 export function Introduction() {
   const theme = useMantineTheme();
@@ -121,8 +141,9 @@ export function Introduction() {
                 </Text>
               </Flex>
               <Image
-                src={profileImage}
+                src="https://res.cloudinary.com/dvhdig9hg/image/upload/v1716976592/01_-_PFP_BW_Keren_xqwdby.jpg"
                 placeholder="blur"
+                blurDataURL={dataUrl}
                 width={200}
                 height={200}
                 alt="Roni Ardiyanto"
