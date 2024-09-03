fb9be052b47d6ca1c1beda7d023f0faff40d5de1
Roni Ardiyanto <roon.ardiyanto@gmail.com>
2024-09-03 15:12:01 +0700

    :zap: feat(app): change experience to card section

--- src/components/features/HomePageFeatures/sections/Experiences/index.tsx
+++ src/components/features/HomePageFeatures/sections/Experiences/index.tsx
@@ -1,11 +1,49 @@
 'use client';
 
-import { Badge, Center, Flex, Text, Timeline, Title } from '@mantine/core';
+import {
+  Badge,
+  Card,
+  Center,
+  Flex,
+  Text,
+  Timeline,
+  Title,
+} from '@mantine/core';
 import { IconBriefcase, IconBriefcaseFilled } from '@tabler/icons-react';
+
+import classes from './styles.module.css';
 
 import { FadeInShow } from '@/components/ui/FadeInShow';
 
 export function Experiences() {
+  const experiences: {
+    title: string;
+    company: string;
+    timeline: string;
+    description: string;
+  }[] = [
+    {
+      title: 'Frontend Developer',
+      company: 'Bank Raya (BRI Group)',
+      timeline: 'Aug 2024 - Now',
+      description:
+        "Work on the Support System (SUS) for Bank Raya to handle, develop, maintain, and fix bugs in the web app for Bank Raya's banking operations.",
+    },
+    {
+      title: 'Frontend Developer',
+      company: 'PT. Optima Media Teknologi',
+      timeline: 'Oct 2022 - Aug 2024',
+      description:
+        'I worked closely with clients to understand their unique needs and create custom solutions that address their specific business challenges, especially with geospatial web applications / GIS.',
+    },
+    {
+      title: 'Frontend Developer Intern',
+      company: 'NAFAS Indonesia',
+      timeline: 'May 2022 - Aug 2022',
+      description:
+        'I worked on dashboard realtime monitoring for air quality data from IOT devices.',
+    },
+  ];
   return (
     <Flex direction="column" gap="md" my="md">
       <Center>
@@ -22,164 +60,57 @@ export function Experiences() {
           </Title>
         </Badge>
       </Center>
-      <Timeline active={1} bulletSize={24} lineWidth={2}>
-        <Timeline.Item
-          bullet={<IconBriefcaseFilled size={12} />}
-          title={
-            <Text size="lg" fw={700} style={{ letterSpacing: '-0.05em' }}>
-              Frontend Developer
-            </Text>
-          }
-        >
-          <Flex
-            direction="row"
-            gap="sm"
-            align="center"
-            w="100%"
-            justify="space-between"
-          >
-            <Text size="md" fw={500}>
-              Bank Raya (BRI Group)
-            </Text>
-            <FadeInShow>
-              <Badge tt="capitalize" variant="outline" p="sm" my="xs">
-                <Text
-                  size="xs"
-                  fw={600}
-                  style={{
-                    letterSpacing: '-0.05em',
-                  }}
+      <Card withBorder w="100%" radius="md" className={classes.card} px="xl">
+        <Flex direction="column" gap="md" my="md">
+          <Timeline active={1} bulletSize={24} lineWidth={2}>
+            {experiences.map((experience, index) => (
+              <Timeline.Item
+                bullet={
+                  index === 0 ? (
+                    <IconBriefcaseFilled size={12} />
+                  ) : (
+                    <IconBriefcase size={12} />
+                  )
+                }
+                title={
+                  <Text size="lg" fw={700} style={{ letterSpacing: '-0.05em' }}>
+                    {experience.title}
+                  </Text>
+                }
+                key={experience.title}
+              >
+                <Flex
+                  direction="row"
+                  gap="sm"
+                  align="center"
+                  w="100%"
+                  justify="space-between"
                 >
-                  Aug 2024 - Now
+                  <Text size="md" fw={500}>
+                    {experience.company}
+                  </Text>
+                  <FadeInShow>
+                    <Badge tt="capitalize" variant="outline" p="sm" my="xs">
+                      <Text
+                        size="xs"
+                        fw={600}
+                        style={{
+                          letterSpacing: '-0.05em',
+                        }}
+                      >
+                        {experience.timeline}
+                      </Text>
+                    </Badge>
+                  </FadeInShow>
+                </Flex>
+                <Text c="dimmed" size="sm">
+                  {experience.description}
                 </Text>
-              </Badge>
-            </FadeInShow>
-          </Flex>
-          <Text c="dimmed" size="sm">
-            Work on the Support System (SUS) for Bank Raya to handle, develop,
-            maintain, and fix bugs in the web app for Bank Raya's banking
-            operations.
-          </Text>
-        </Timeline.Item>
-
-        <Timeline.Item
-          bullet={<IconBriefcaseFilled size={12} />}
-          title={
-            <Text size="lg" fw={700} style={{ letterSpacing: '-0.05em' }}>
-              Frontend Developer
-            </Text>
-          }
-        >
-          <Flex
-            direction="row"
-            gap="sm"
-            align="center"
-            w="100%"
-            justify="space-between"
-          >
-            <Text size="md" fw={500}>
-              PT. Optima Media Teknologi
-            </Text>
-            <FadeInShow>
-              <Badge tt="capitalize" variant="outline" p="sm" my="xs">
-                <Text
-                  size="xs"
-                  fw={600}
-                  style={{
-                    letterSpacing: '-0.05em',
-                  }}
-                >
-                  Oct 2022 - Aug 2024
-                </Text>
-              </Badge>
-            </FadeInShow>
-          </Flex>
-          <Text c="dimmed" size="sm">
-            I work closely with clients to understand their unique needs and
-            create custom solutions that address their specific business
-            challenges, especially with geospatial web applications / GIS.
-          </Text>
-        </Timeline.Item>
-
-        <Timeline.Item
-          bullet={<IconBriefcase size={12} />}
-          title={
-            <Text size="lg" fw={700} style={{ letterSpacing: '-0.05em' }}>
-              Frontend Developer Intern
-            </Text>
-          }
-        >
-          <Flex
-            direction="row"
-            gap="sm"
-            align="center"
-            w="100%"
-            justify="space-between"
-          >
-            <Text size="md" fw={500}>
-              NAFAS Indonesia
-            </Text>
-            <FadeInShow>
-              <Badge tt="capitalize" variant="outline" p="sm" my="xs">
-                <Text
-                  size="xs"
-                  fw={600}
-                  style={{
-                    letterSpacing: '-0.05em',
-                  }}
-                >
-                  May 2022 - Aug 2022{' '}
-                </Text>
-              </Badge>
-            </FadeInShow>
-          </Flex>
-          <Text c="dimmed" size="sm">
-            I worked on dashboard realtime monitoring for air quality data from
-            IOT devices.
-          </Text>
-        </Timeline.Item>
-
-        <Timeline.Item
-          title={
-            <Text size="lg" fw={700} style={{ letterSpacing: '-0.05em' }}>
-              Frontend Developer Intern
-            </Text>
-          }
-          bullet={<IconBriefcase size={12} />}
-          lineVariant="dashed"
-        >
-          <Flex
-            direction="row"
-            gap="sm"
-            align="center"
-            w="100%"
-            justify="space-between"
-          >
-            <Text size="md" fw={500}>
-              GoTo Impact
-            </Text>
-            <FadeInShow>
-              <Badge tt="capitalize" variant="outline" p="sm" my="xs">
-                <Text
-                  size="xs"
-                  fw={600}
-                  style={{
-                    letterSpacing: '-0.05em',
-                  }}
-                >
-                  Feb 2022 - Aug 2022
-                </Text>
-              </Badge>
-            </FadeInShow>
-          </Flex>
-
-          <Text c="dimmed" size="sm">
-            Program from YABB by Gojek & Tokopedia Explore and learn more about
-            frontend developers using React.js Selected as GIGIH Interns at YABB
-            Partner.
-          </Text>
-        </Timeline.Item>
-      </Timeline>
+              </Timeline.Item>
+            ))}
+          </Timeline>
+        </Flex>
+      </Card>
     </Flex>
   );
 }
